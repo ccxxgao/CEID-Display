7a99ace5065c3e43eab79505d1968550c0f279cf
dante <ceid@yale.edu>
2018-09-24 15:57:59 -0400

    trying to fix the calendar

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~/CEID-Display/
+sleep 60		# give it a minute to get internet
 python3 ./server.py &
-sleep 3
+sleep 3			# give it a few seconds to start the server
 chromium-browser --app=http://localhost:5000 --start-fullscreen &
