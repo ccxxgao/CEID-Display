a7b068c77ded7ed8a432ebeb4914b34cffbacad9
dante <ceid@yale.edu>
2018-09-28 13:21:37 -0400

    added extra sleep and server launch lines

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+sleep 3
 cd ~/CEID-Display/
 python3 ./server.py &
 sleep 3
-chromium-browser --app=http://localhost:5000 --start-fullscreen &
+python3 ./server.py &
+sleep 3
+chromium-browser --app=http://localhost:5000 --start-fullscreen
